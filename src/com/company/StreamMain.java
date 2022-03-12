40e4dbd4ab21bc45c69c8d843df74983002d154c
Oleg.Nee <89423263+olegnee@users.noreply.github.com>
2022-03-13 03:05:51 +0900

    edited homework

--- src/com/company/StreamMain.java
+++ src/com/company/StreamMain.java
@@ -6,9 +6,9 @@ import java.util.stream.*;
 public class StreamMain {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        Stream<Integer> stream = list.stream();
-            stream.
-                filter(x -> x > 0 && x % 2 == 0).
+        list.stream().
+                filter(x -> x > 0).
+                    filter(x -> x % 2 == 0).
                 sorted(Comparator.naturalOrder()).
                     forEach(System.out::println);
     }
